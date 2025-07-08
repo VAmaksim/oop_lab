a7592b1d7011e0abf182c2626da06ff986342d49
VAmaksim <m.voronin56@gmail.com>
2025-07-08 15:44:05 +0500

    Update and rename font.json to json_to_txt.py

---
+++ lab2/json_to_txt.py
@@ -0,0 +1,10 @@
+import json
+
+with open('font.json', 'r') as file:
+    info = json.load(file)
+with open('font.txt', 'w') as file:
+    for sym in info.values():
+        for line in sym:
+            file.write(line)
+            file.write('\n')
+        file.write('-' * 15 + '\n')
